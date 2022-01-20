9eca77dbaaf5395df580526c75fde761178a6f92
Luis <luismartinezmilla@gmail.com>
2022-01-20 10:03:43 +0100

    register y login

--- app/Http/Controllers/InkmeController.php
+++ app/Http/Controllers/InkmeController.php
@@ -14,39 +14,75 @@ use Illuminate\Support\Str;
 class InkmeController extends Controller
 {
 
-    public function register(Request $req){ //Pide: api_token, name, email, password, profile_picture y location
+    public function register(Request $req){ //Pide: api_token, name, email, password, y numtlf || Devuelve: status, msg y usuario_id
         $jdata = $req->getContent();
         $data = json_decode($jdata);
+        $response["status"]=1;
 
-        if($data->name && $data->email && $data->password && $data->profile_picture && $data->location){
-            try{
+        
+        try{
+            if($data->name && $data->email && $data->password && $data->numtlf){
                 $user = new Usuario;
                 $user->name = $data->name;
-                $user->email = $data->email;
+                $user->email = $data->email;//validar email
                 if(preg_match("/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{6,}/", $data->password)){
                     $user->password = Hash::make($data->password);
                 }else{
-                    $response["status"]=0;
-                    $response["msg"]="Contraseña insegura. Mínimo: 1 Mayúscula, 1 minúscula, 1 caracter especial y 1 número";
-                    return response()->json($response);
+                    throw new Exception("Contraseña insegura. Mínimo: 1 Mayúscula, 1 minúscula, 1 caracter especial y 1 número");
                 }
-                $user->profile_picture = $data->profile_picture;
-                $user->location = $data->location;
+                if($data->numtlf >= 100000000 && $data->numtlf <= 999999999){
+                    $user->numtlf = $data->numtlf;
+                }else{
+                    throw new Exception("Número de teléfono debe ser de 9 dígitos");
+                }
                 $user->views = 0;
                 $user->save();
-                $response["status"]=1;
                 $response["msg"]="Guardado con éxito";
-            }catch(\Exception $e){
-                $response["status"]=0;
-                $response["msg"]="Error al intentar guardar el usuario: ".$e;
+                $response["usuario_id"]=$user->id;
+            }else{
+                throw new Exception("Introduce name, email, password, profile_picture y location");
             }
-            
-        }else{
+        }catch(\Exception $e){
             $response["status"]=0;
-            $response["msg"]="introduce name, email, password, profile_picture y location";
+            $response["msg"]="Error al intentar guardar el usuario: ".$e->getMessage();
         }
-
         
         return response()->json($response);
     }
+    public function login(Request $req){ //Pide: api_token, email y password || Devuelve: "status" "msg" y "api_token" (si ha iniciado bien)
+        $jdata = $req->getContent();
+        $data = json_decode($jdata);
+
+        $response["status"]=1;
+        try{
+            if($data->email && $data->password){
+                //comprobar email
+                $user = User::where('email', $data->email)->first();
+                if ($user == null){
+                    throw new Exception("Error: Email no existe");
+                }
+                //comprobar que la contraseña coincide con la asociada al email
+                if(!Hash::check($data->password, $user->password)){
+                    throw new Exception("Contraseña incorrecta");
+                }                
+                //crear token y guardarlo
+                $allTokens = User::pluck('api_token')->toArray();
+                do {
+                    $user->api_token = Hash::make(now().$user->email);
+                } while (in_array($user->api_token, $allTokens)); //En bucle mientras que el apitoken esté duplicado
+                $user->save();
+                //responder con token
+                $response["msg"]="Sesion iniciada";
+                $response["api_token"]=$user->api_token;
+
+            }else{
+                throw new Exception("Error: Introduce email y password");
+            }
+        }catch(\Exception $e){
+            $response["status"]=0;
+            $response["msg"]=$e->getMessage();
+        }
+        return response()->json($response);
+    }
+
 }
--- database/migrations/2022_01_13_132355_create_usuarios_table.php
+++ database/migrations/2022_01_13_132355_create_usuarios_table.php
@@ -18,10 +18,11 @@ class CreateUsuariosTable extends Migration
             $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('profile_picture');
+            $table->integer('numtlf');
+            $table->string('profile_picture')->nullable();
             $table->string('api_token')->unique()->nullable();
-            $table->string('location');
-            $table->integer('views');
+            $table->string('location')->nullable();
+            $table->integer('views')->nullable(); //Dicen de quitar esto
             $table->integer('estudio_id')->nullable();
             $table->timestamps();
         });
